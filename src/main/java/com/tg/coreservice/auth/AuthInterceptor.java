21325d2bb22d99b221f6ee090b90f359073b2411
JinuCheon <woojin8787@gmail.com>
2023-04-30 01:47:59 +0900

    feat: 세션 인증을 위한 스프링 인터셉터 구현

---
+++ src/main/java/com/tg/coreservice/auth/AuthInterceptor.java
@@ -0,0 +1,32 @@
+package com.tg.coreservice.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Configuration
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
+        if (auth == null) { // Auth 어노테이션이 없으면, 인증이 필요없는 요청이므로 true를 반환한다.
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        if (session.getAttribute("userId") == null) {
+            response.sendError(HttpStatus.UNAUTHORIZED.value(), "로그인이 필요합니다.");
+            return false;
+        }
+
+        Long userId = (Long) session.getAttribute("userId");
+        UserContext.CONTEXT.set(userId);
+        return true;
+    }
+}
--- src/main/java/com/tg/coreservice/controller/UserController.java
+++ src/main/java/com/tg/coreservice/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.tg.coreservice.controller;
 
+import com.tg.coreservice.auth.Auth;
+import com.tg.coreservice.auth.UserContext;
 import com.tg.coreservice.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -22,5 +24,19 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Auth
+    @GetMapping("/user")
+    public ResponseEntity<Void> testAuthUser() {
+        Long userId = UserContext.getContext();
+        System.out.println("로그인한 사용자만 접근 가능한 컨트롤러");
+        System.out.println("userId = " + userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/guest")
+    public ResponseEntity<Void> testNoAuthUser() {
+        System.out.println("Auth 어노테이션이 붙지 않은 컨트롤러는 비로그인한 사용자도 접근 가능");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
 
