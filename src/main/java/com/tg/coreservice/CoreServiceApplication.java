a35b556fd8286f630dea57ebb8259394af6f94f4
JinuCheon <woojin8787@gmail.com>
2023-04-30 01:42:36 +0900

    feat: 카카오 OAuth2.0 API 통신 스팩 정의

--- src/main/java/com/tg/coreservice/CoreServiceApplication.java
+++ src/main/java/com/tg/coreservice/CoreServiceApplication.java
@@ -1,9 +1,14 @@
 package com.tg.coreservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
+@EnableFeignClients("com.tg.coreservice.client")
 public class CoreServiceApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/tg/coreservice/client/KakaoAccessTokenFeignClient.java
@@ -0,0 +1,20 @@
+package com.tg.coreservice.client;
+
+import com.tg.coreservice.dto.KakaoAccessTokenResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "KakaoFeignClient", url = "${kakao.api_url.token}")
+public interface KakaoAccessTokenFeignClient {
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    KakaoAccessTokenResponseDto call(
+            @RequestHeader("Content-Type") String contentType,
+            @RequestParam("grant_type") String grantType,
+            @RequestParam("client_id") String clientId,
+            @RequestParam("redirect_uri") String redirectUri,
+            @RequestParam("code") String code
+    );
+}
